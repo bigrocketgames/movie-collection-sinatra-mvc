722786f26d6d0bd01f3464e724952ee19da258eb
NATHAN ROBBERT <admin@bigrocketgames.com>
2016-12-24 23:33:48 +0000

    Added views with default templates.  Added routes in controller.  Added notes for todos.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,30 @@ class ApplicationController < Sinatra::Base
     erb :'index'
   end
 
+  post '/' do
+
+    redirect '/collection'
+  end
+
+  get '/signup' do
+    erb :'new'
+  end
+
+  get '/login' do
+
+  end
+
+  get '/collection' do
+
+  end
+
+  post '/collection' do
+
+    redirect '/collection'
+  end
+
+  get '/movie/:id' do
+
+  end
+
 end
---
+++ notes.md
@@ -0,0 +1,6 @@
+Use table to show collection.
+-name
+-summary
+-delete link
+
+add form below collection to add a new movie.
