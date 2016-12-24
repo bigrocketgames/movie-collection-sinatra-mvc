8e2f5d28fb97ee46778e4dc45e5b3f06816f7b9f
NATHAN ROBBERT <admin@bigrocketgames.com>
2016-12-24 22:23:43 +0000

    Got errors fixed that weren't allowing rackup or shotgun to work properly.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
-
+  register Sinatra::ActiveRecordExtension
   configure do
     enable :sessions
     set :session_secret, "secret"
     set :views, 'app/views'
+  end
+
+  get '/' do
+    puts "We made it to the index."
+    erb :'index'
   end
 
 end
