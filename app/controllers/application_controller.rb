25521b2bab45d95b4338645e560c3283ff3713ad
NATHAN ROBBERT <admin@bigrocketgames.com>
2016-12-29 04:51:33 +0000

    Added ability to delete movie from collection, removed movie id from table and turned name to link to show single movie page, added slug methods to movie class, updated collection erb and single erb to look better.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < Sinatra::Base
     @user = User.new(params[:user])
     if @user.save
       session[:user_id] = @user.id
-      redirect '/collection'
+      redirect "/user/#{@user.slug}/collection"
     else
       flash[:message] = "Failed to signup new user.  Please make sure all fields are filled in."
       redirect "/signup"
@@ -23,7 +23,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/signup' do
-    erb :'new'
+    logged_in? ? (redirect "/user/#{current_user.slug}/collection") : (erb :'new')
   end
 
   get '/login' do
@@ -34,29 +34,43 @@ class ApplicationController < Sinatra::Base
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect '/collection'
+      redirect "/user/#{@user.slug}/collection"
     else
       flash[:message] = "Invalid username/password.  Please try again."
       redirect '/login'
     end
   end
 
-  get '/collection' do
+  get '/user/:slug/collection' do
     if logged_in?
-      @user = current_user
+      @user = User.find_by_slug(params[:slug])
       erb :'collection'
     else
       redirect '/login'
     end
   end
 
-  post '/collection' do
+  post '/user/:slug/collection' do
     @movie = Movie.create(name: params[:movie][:name], summary: params[:movie][:summary], user_id: session[:user_id])
-    redirect '/collection'
+    flash[:message] = "Movie successfully added to collection."
+    redirect "/user/#{current_user.slug}/collection"
   end
 
-  get '/movie/:id' do
+  get '/movie/:slug' do
+    @movie = Movie.find_by_slug(params[:slug])
+    erb :'single'
+  end
 
+  get '/movie/:slug/delete' do
+    @movie = Movie.find_by_slug(params[:slug])
+    if current_user.id == @movie.user_id
+      @movie.destroy
+      flash[:message] = "Movie successfully removed from collection."
+      redirect "/user/#{current_user.slug}/collection"
+    else
+      flash[:message] = "You are not authorized to edit this collection."
+      redirect "/user/#{@movie.user.slug}/collection"
+    end
   end
 
   helpers do
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,11 @@
 class Movie < ActiveRecord::Base
   belongs_to :user
 
+  def slug
+    self.name.gsub(" ", "-")
+  end
 
+  def self.find_by_slug(slug)
+    self.find_by(name: slug.gsub("-", " "))
+  end
 end
