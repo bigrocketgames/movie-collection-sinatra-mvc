f9e3b2fb409269a13bef05cf374a2cca2c31afea
NATHAN ROBBERT <admin@bigrocketgames.com>
2017-01-04 04:12:38 +0000

    Changed some routes to the user controller from application controller.  Edited erb pages with links to signup page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,29 +15,6 @@ class ApplicationController < Sinatra::Base
     erb :'index'
   end
 
-  post '/' do
-    @user_found = User.find_by(username: params[:user][:username]) ? true : false
-
-    if @user_found
-      flash[:message] = "This username already exists.  Please select a new one."
-      redirect back
-    else
-      @user = User.new(params[:user])
-      if @user.save
-        session[:user_id] = @user.id
-        flash[:message] = "You have successfully created an account.  Here is you new collection page.  You can now add movies to your collection."
-        redirect "/user/#{@user.slug}/collection"
-      else
-        flash[:message] = "Failed to signup new user.  Please make sure all fields are filled in."
-        redirect "/signup"
-      end
-    end
-  end
-
-  get '/signup' do
-    logged_in? ? (redirect "/user/#{current_user.slug}/collection") : (erb :'new')
-  end
-
   get '/login' do
     erb :'login'
   end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,28 @@
 class UserController < ApplicationController
 
+  post '/user' do
+    @user_found = User.find_by(username: params[:user][:username]) ? true : false
+    binding.pry
+    if @user_found
+      flash[:message] = "This username already exists.  Please select a new one."
+      redirect back
+    else
+      @user = User.new(params[:user])
+      if @user.save
+        session[:user_id] = @user.id
+        flash[:message] = "You have successfully created an account.  Here is you new collection page.  You can now add movies to your collection."
+        redirect "/user/#{@user.slug}/collection"
+      else
+        flash[:message] = "Failed to signup new user.  Please make sure all fields are filled in."
+        redirect "/user/new"
+      end
+    end
+  end
+
+  get '/user/new' do
+    logged_in? ? (redirect "/user/#{current_user.slug}/collection") : (erb :'user/new')
+  end
+
   get '/user/:slug/collection' do
     @logged = logged_in?
 
